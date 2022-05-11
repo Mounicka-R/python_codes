a379a434e97c0b19037449cd283a6223b758f61d
Mounicka-R <81553827+Mounicka-R@users.noreply.github.com>
2022-05-11 16:07:46 +0530

    Add files via upload

---
+++ april_18_set_1.py
@@ -0,0 +1,66 @@
+# 1) create a dict called contact use Global variable for dict
+#    store the mobile number as key 
+#    name and email as value - list example:
+#    9661425678 : ['Ganesh Prasad','ganesh@gmail.com']
+#    9535354450 : ['Latha Srinivas','latha.srinivas@yahoo.com']
+#    ..... and so on...
+#    first display the menu as following:
+#    1. add new mobile details
+#    2. view all the details from contacts
+#    3. view the details of the specific mobile
+#       if that mobile exists else display
+#       error message
+#    4. remove the mobile number from contact
+#    5. edit the details based on the mobile
+#       number. correcting the email id
+#       only.
+#    6. EXIT
+#    enter the choice:
+# on EXIT convert the contact to JSON string 
+# make sure you write 5 functions
+# add_new_contacts()
+# view_all_contacts()
+# view_a_contact(mobile)
+# delete_contact(mobile) - returns True if successfull delete else False
+# modify_contact(mobile)
+
+# class phone_detail():
+#     phone=int(input("enter the phone number: "))
+#     name=input("enter the name: ")
+#     email=input("enter the email: ")
+#     def add_new_contacts(self):
+#         self.mobile={}
+#         while True:
+#             global phone
+#             global name
+#             global email
+#             phone=input("enter the phone number: ")
+#             if len(phone)==10:
+#                 name=input("enter the name: ")
+#                 email=input("enter the email: ")
+#                 self.mobile.update({phone:[name,email]})
+#             else:
+#                 print("please enter 10 digit number")
+#                 break
+#     def view_all_contacts(self):
+#         print(self.mobile)
+#     def view_a_contacts(self):
+#         phone1=input("enter the number to check: ")
+#         for i,j in 
+#         if phone1 in self.mobile.keys():
+#             print
+#         else:
+#             print("number does not exist")
+            
+# a=phone_detail()
+# a.add_new_contacts()
+# a.view_all_contacts()
+# a.view_a_contacts()
+            
+
+
+
+
+
+
+
