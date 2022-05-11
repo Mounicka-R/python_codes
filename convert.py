5f8394690ffddc60df301e0c1135bfbc913e88cd
Mounicka-R <81553827+Mounicka-R@users.noreply.github.com>
2022-05-11 16:13:26 +0530

    Add files via upload

---
+++ convert.py
@@ -0,0 +1,18 @@
+# input two strings like this  '1234'  and '4567'
+# convert them to int , add them
+# apply the same using float   say if the input is '45.33' and '234.99'
+# add them converting to float
+
+a=(input("enter the number:"))
+b=(input("enter the number:"))
+sum=int(a)+int(b)
+print("sum of 2 number is:",sum)
+
+c=(input("enter the number:"))
+d=(input("enter the number:"))
+sum1=float(c)+float(d)
+print("sumof 2 number is:",sum1)
+
+
+
+
